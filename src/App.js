4e2cd7f2f189a0aee9816686c0b4f5f64968d027
Chris Paterson <chrispaterson22@gmail.com>
2021-05-10 16:47:07 +0100

    very basic into to React app done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import TodoItem from './components/Todo'
+
 function App() {
-	return <div>Hello!</div>
+	return (
+		<div>
+			<h1>My Todos</h1>
+			<TodoItem text='Learn React' />
+			<TodoItem text='Master React' />
+			<TodoItem text='Explore the full React course' />
+		</div>
+	)
 }
 
 export default App
